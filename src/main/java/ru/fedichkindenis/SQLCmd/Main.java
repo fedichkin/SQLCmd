0041d79444a5f5e5b496a467495977ef6b608e9b
Fedichkin Denis <fedichkin.dy@gmail.com>
2016-06-05 20:35:50 +0300

    start project

---
+++ src/main/java/ru/fedichkindenis/SQLCmd/Main.java
@@ -0,0 +1,12 @@
+package ru.fedichkindenis.SQLCmd;
+
+/**
+ * Created by Денис on 05.06.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
