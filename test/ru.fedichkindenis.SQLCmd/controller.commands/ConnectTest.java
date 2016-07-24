48e52167de5480917ec958acd029ccfc60156308
Fedichkin Denis <fedichkin.dy@gmail.com>
2016-07-24 22:22:55 +0300

    add test for command Connect

---
+++ test/ru.fedichkindenis.SQLCmd/controller.commands/ConnectTest.java
@@ -0,0 +1,45 @@
+package ru.fedichkindenis.SQLCmd.controller.commands;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import ru.fedichkindenis.SQLCmd.controller.Commands.Command;
+import ru.fedichkindenis.SQLCmd.controller.Commands.Connect;
+import ru.fedichkindenis.SQLCmd.model.DBManager;
+import ru.fedichkindenis.SQLCmd.view.View;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Денис on 23.07.2016.
+ *
+ * Тесты для команды connect
+ */
+
+public class ConnectTest {
+
+    DBManager manager;
+    View view;
+    Command command;
+
+    @Before
+    public void setup() {
+        manager = mock(DBManager.class);
+        view = mock(View.class);
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        try {
+            command = new Connect(manager, view, "connect|d|12|fg|ere");
+            command.execute();
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Указан не верный формат команды", e.getMessage());
+        }
+    }
+}
