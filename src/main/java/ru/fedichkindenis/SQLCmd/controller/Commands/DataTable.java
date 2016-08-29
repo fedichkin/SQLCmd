07ffd7e2e61ca0585b71618e9cbc1c83dc2b3fe9
Fedichkin Denis <fedichkin.dy@gmail.com>
2016-08-29 22:56:48 +0300

    command dataTable

---
+++ src/main/java/ru/fedichkindenis/SQLCmd/controller/Commands/DataTable.java
@@ -0,0 +1,48 @@
+package ru.fedichkindenis.SQLCmd.controller.Commands;
+
+import ru.fedichkindenis.SQLCmd.model.DBManager;
+import ru.fedichkindenis.SQLCmd.model.DataMap;
+import ru.fedichkindenis.SQLCmd.util.StringUtil;
+import ru.fedichkindenis.SQLCmd.view.ViewDecorator;
+
+import java.util.List;
+
+/**
+ * Команда для вывода данных таблицы
+ * Формат команды: data-table|nameTable
+ * Пример команды: data-table|usr
+ */
+public class DataTable implements Command {
+
+    private DBManager dbManager;
+    private ViewDecorator view;
+    private String textCommand;
+
+    private static int COUNT_ARGUMENT = 2;
+
+    public DataTable(DBManager dbManager, ViewDecorator view, String textCommand) {
+        this.dbManager = dbManager;
+        this.view = view;
+        this.textCommand = textCommand;
+    }
+
+    @Override
+    public void execute() {
+
+        if(!validateCommand()) {
+            throw new IllegalArgumentException("Указан не верный формат команды");
+        }
+
+        String nameTable = textCommand.split("\\|")[1];
+
+        List<DataMap> dataMapList = dbManager.dataTable(nameTable);
+        view.write(dataMapList);
+    }
+
+    private boolean validateCommand() {
+
+        return !StringUtil.isEmpty(textCommand)
+                && textCommand.startsWith("data-table|")
+                && textCommand.split("\\|").length == COUNT_ARGUMENT;
+    }
+}
