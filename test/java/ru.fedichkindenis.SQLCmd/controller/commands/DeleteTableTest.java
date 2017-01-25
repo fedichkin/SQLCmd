1fdf00a8b378bef03155fd7f451e38e6e7f16db7
Fedichkin Denis <4javaMeetUp@gmail.com>
2017-01-25 21:47:54 +0200

    add test for command delete-table

---
+++ test/java/ru.fedichkindenis.SQLCmd/controller/commands/DeleteTableTest.java
@@ -0,0 +1,65 @@
+package ru.fedichkindenis.SQLCmd.controller.commands;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockitoAnnotations;
+import ru.fedichkindenis.SQLCmd.controller.Commands.Command;
+import ru.fedichkindenis.SQLCmd.controller.Commands.DeleteTable;
+import ru.fedichkindenis.SQLCmd.controller.Commands.ListTable;
+import ru.fedichkindenis.SQLCmd.model.DBManager;
+import ru.fedichkindenis.SQLCmd.view.ViewDecorator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Класс для тестирования команды delete-table
+ */
+public class DeleteTableTest implements CommandTest {
+
+    private DBManager dbManager;
+    private ViewDecorator viewDecorator;
+    private Command command;
+
+    @Override
+    @Before
+    public void setup() {
+
+        MockitoAnnotations.initMocks(this);
+
+        dbManager = mock(DBManager.class);
+        viewDecorator = mock(ViewDecorator.class);
+    }
+
+    @Override
+    @Test
+    public void testIncorrectCommandFormat() {
+
+        try {
+            command = new ListTable(dbManager, viewDecorator, "delete-table|");
+            command.execute();
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Указан не верный формат команды", e.getMessage());
+        }
+    }
+
+    @Override
+    public void testCorrectCommandFormat() {
+
+        command = new DeleteTable(dbManager, viewDecorator, "delete-table|user-info");
+        command.execute();
+
+        shouldPrintView("[Таблица user-info была удалена!]");
+    }
+
+    private void shouldPrintView(String expected) {
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(viewDecorator, atLeastOnce()).write(captor.capture());
+        assertEquals(expected, captor.getAllValues().toString());
+    }
+}
