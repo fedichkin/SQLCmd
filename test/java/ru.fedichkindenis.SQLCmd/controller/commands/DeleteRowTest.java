3094468b596bb6fb3d285936f51c5bb663a8c7a6
Fedichkin Denis <fedichkin.dy@gmail.com>
2017-01-27 23:40:28 +0200

    add test for command delete row

---
+++ test/java/ru.fedichkindenis.SQLCmd/controller/commands/DeleteRowTest.java
@@ -0,0 +1,66 @@
+package ru.fedichkindenis.SQLCmd.controller.commands;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockitoAnnotations;
+import ru.fedichkindenis.SQLCmd.controller.Commands.Command;
+import ru.fedichkindenis.SQLCmd.controller.Commands.DeleteRow;
+import ru.fedichkindenis.SQLCmd.model.DBManager;
+import ru.fedichkindenis.SQLCmd.view.ViewDecorator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Класс для тестирования команды delete-row
+ */
+public class DeleteRowTest implements CommandTest {
+
+    private DBManager dbManager;
+    private ViewDecorator viewDecorator;
+    private Command command;
+
+    @Override
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        dbManager = mock(DBManager.class);
+        viewDecorator = mock(ViewDecorator.class);
+    }
+
+    @Override
+    @Test
+    public void testIncorrectCommandFormat() {
+
+        try {
+            command = new DeleteRow(dbManager, viewDecorator,
+                    "delete-row|user|!IF|id|4");
+            command.execute();
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Указан не верный формат команды", e.getMessage());
+        }
+    }
+
+    @Override
+    @Test
+    public void testCorrectCommandFormat() {
+
+        command = new DeleteRow(dbManager, viewDecorator,
+                "delete-row|usr|!IF|id|=|4");
+        command.execute();
+
+        shouldPrintView("[В таблице usr были удалены строки!]");
+    }
+
+    private void shouldPrintView(String expected) {
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(viewDecorator, atLeastOnce()).write(captor.capture());
+        assertEquals(expected, captor.getAllValues().toString());
+    }
+}
