4c0e1622f0ade1d76004038d0684684d504f46c5
Fedichkin Denis <fedichkin.dy@gmail.com>
2017-02-04 23:02:20 +0200

    add test for command user-query

--- test/java/ru.fedichkindenis.SQLCmd/controller/commands/CommandFactoryTest.java
+++ test/java/ru.fedichkindenis.SQLCmd/controller/commands/CommandFactoryTest.java
@@ -15,6 +15,7 @@ import ru.fedichkindenis.SQLCmd.controller.Commands.UpdateRow;
 import ru.fedichkindenis.SQLCmd.controller.Commands.DeleteRow;
 import ru.fedichkindenis.SQLCmd.controller.Commands.DataTable;
 import ru.fedichkindenis.SQLCmd.controller.Commands.CreateTable;
+import ru.fedichkindenis.SQLCmd.controller.Commands.UserQuery;
 import ru.fedichkindenis.SQLCmd.model.DBManager;
 import ru.fedichkindenis.SQLCmd.view.ViewDecorator;
 
@@ -59,10 +60,10 @@ public class CommandFactoryTest implements CommandTest {
 
         String[] textCommands = {"connect|1|1|1|1|1", "exit", "list-table", "bla",
                 "clear-table|usr", "delete-table|usr", "insert-row|usr", "update-row|usr",
-                "delete-row|usr", "data-table|usr", "create-table|usr"};
+                "delete-row|usr", "data-table|usr", "create-table|usr", "user-query|insert"};
         Class[] commands = {Connect.class, Exit.class, ListTable.class, Unsupported.class,
                 ClearTable.class, DeleteTable.class, InsertRow.class, UpdateRow.class,
-                DeleteRow.class, DataTable.class, CreateTable.class};
+                DeleteRow.class, DataTable.class, CreateTable.class, UserQuery.class};
 
         for(int index = 0; index < textCommands.length; index++) {
 
---
+++ test/java/ru.fedichkindenis.SQLCmd/controller/commands/UserQueryTest.java
@@ -0,0 +1,67 @@
+package ru.fedichkindenis.SQLCmd.controller.commands;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockitoAnnotations;
+import ru.fedichkindenis.SQLCmd.controller.Commands.Command;
+import ru.fedichkindenis.SQLCmd.controller.Commands.CreateTable;
+import ru.fedichkindenis.SQLCmd.controller.Commands.UserQuery;
+import ru.fedichkindenis.SQLCmd.model.DBManager;
+import ru.fedichkindenis.SQLCmd.view.ViewDecorator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Класс для тестирования команды user-query
+ */
+public class UserQueryTest implements CommandTest {
+
+    private DBManager dbManager;
+    private ViewDecorator viewDecorator;
+    private Command command;
+
+    @Override
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        dbManager = mock(DBManager.class);
+        viewDecorator = mock(ViewDecorator.class);
+    }
+
+    @Override
+    @Test
+    public void testIncorrectCommandFormat() {
+
+        try {
+            command = new UserQuery(dbManager, viewDecorator,
+                    "user-query|insert into usr (id, login, password) values(6, 'us', '1234')");
+            command.execute();
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Указан не верный формат команды", e.getMessage());
+        }
+    }
+
+    @Override
+    @Test
+    public void testCorrectCommandFormat() {
+
+        command = new UserQuery(dbManager, viewDecorator,
+                "user-query|<insert into usr (id, login, password) values(6, 'us', '1234')");
+        command.execute();
+
+        shouldPrintView("[Была выполнен запрос: insert into usr (id, login, password) values(6, 'us', '1234')!]");
+    }
+
+    private void shouldPrintView(String expected) {
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(viewDecorator, atLeastOnce()).write(captor.capture());
+        assertEquals(expected, captor.getAllValues().toString());
+    }
+}
